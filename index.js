4a43003aceacabd54b7f29ee6d1a46a261c46826
PRABHAT CHANDRA DWIVEDI <mr.prabhatdwivedi@gmail.com>
2024-03-13 14:00:22 +0530

    Changing allowed origin to all

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:4000",
+    origin: "*",
   },
 });
 const roomChoices ={};
