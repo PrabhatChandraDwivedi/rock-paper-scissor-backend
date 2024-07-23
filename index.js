231a601c9815bb42c200bbb30bda6e5f49e9b313
PRABHAT CHANDRA DWIVEDI <mr.prabhatdwivedi@gmail.com>
2024-07-23 19:13:24 +0530

    Final index.js

--- index.js
+++ index.js
@@ -17,16 +17,23 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log(socket.id);
+  console.log(socket.rooms);
+
   socket.on("create-room", (roomName, callback) => {
     socket.join(roomName);
     roomChoices[roomName] = {};
     roomChoices[roomName][socket.id] = null;
+    console.log("user joined " + roomName);
     callback(true);
   });
 
   socket.on("join-room", (roomName, callback) => {
     let numberofUsers = io.sockets.adapter.rooms.get(roomName, callback);
     if (numberofUsers?.size === undefined || numberofUsers?.size < 2) {
+      console.log(socket.id);
+      socket.join(roomName);
       roomChoices[roomName][socket.id] = null;
       const firstPlayerSocketId = Object.keys(roomChoices[roomName])[0];
       const secondPlayerSocketId = Object.keys(roomChoices[roomName])[1];
@@ -39,18 +46,25 @@ io.on("connection", (socket) => {
           secondPlayerSocketId,
           firstPlayerSocketId
         );
+        console.log("==========================");
         io.to(socket.id).emit("start-game-immediately");
         callback(true);
       }
+      console.log("user joined " + roomName);
     } else if (numberofUsers && numberofUsers.size === 2) {
+      console.log("Room is full");
       callback(false);
     } else {
+      console.log(roomChoices);
+      console.log(numberofUsers?.size);
       callback(false);
     }
   });
 
   //game logic goes here
   socket.on("player-choice", ({ roomName, choice }) => {
+    console.log("&&&&&");
+    console.log(roomName + choice);
     roomChoices[roomName][socket.id] = choice;
     const firstPlayerSocketId = Object.keys(roomChoices[roomName])[0];
     const secondPlayerSocketId = Object.keys(roomChoices[roomName])[1];
@@ -58,8 +72,10 @@ io.on("connection", (socket) => {
       roomChoices[roomName][firstPlayerSocketId] !== null &&
       roomChoices[roomName][secondPlayerSocketId] !== null
     ) {
+      // Both players have made their choices
       const firstPlayerChoice = roomChoices[roomName][firstPlayerSocketId];
       const secondPlayerChoice = roomChoices[roomName][secondPlayerSocketId];
+      // Compare choices and determine the winner
       let winner;
       if (firstPlayerChoice === secondPlayerChoice) {
         winner = "It's a tie!";
@@ -72,7 +88,10 @@ io.on("connection", (socket) => {
       } else {
         winner = "Player 2 wins!";
       }
+      // Emit the winner to both players
+      console.log(winner);
       io.to(roomName).emit("game-result", winner);
+      // Reset choices for the next round
       roomChoices[roomName][firstPlayerSocketId] = null;
       roomChoices[roomName][secondPlayerSocketId] = null;
     }
